a3344e6920a8a45e450a6a7aeac592bae425bf93
Liebus <e.eduards@inbox.lv>
2023-06-06 08:26:38 +0300

    Pievienoju parejos assets

--- Assets/Skripti/Objekti.cs
+++ Assets/Skripti/Objekti.cs
@@ -6,6 +6,15 @@ public class Objekti : MonoBehaviour {
 	public GameObject atkritumuMasina;
 	public GameObject atraPalidziba;
 	public GameObject autobuss;
+	public GameObject b2;
+	public GameObject cementaMasina;
+	public GameObject e46;
+	public GameObject e61;
+	public GameObject ekskavators;
+	public GameObject policija;
+	public GameObject traktors;
+	public GameObject traktors2;
+	public GameObject ugunsdzeseji;
 
 	[HideInInspector]
 	public Vector2 atkrMKoord;
@@ -13,6 +22,24 @@ public class Objekti : MonoBehaviour {
 	public Vector2 atrPKoord;
 	[HideInInspector]
 	public Vector2 bussKoord;
+	[HideInInspector]
+	public Vector2 b2Koord;
+	[HideInInspector]
+	public Vector2 cemenMKoord;
+	[HideInInspector]
+	public Vector2 e46Koord;
+	[HideInInspector]
+	public Vector2 e61Koord;
+	[HideInInspector]
+	public Vector2 ekskavKoord;
+	[HideInInspector]
+	public Vector2 policijaKoord;
+	[HideInInspector]
+	public Vector2 traktorsKoord;
+	[HideInInspector]
+	public Vector2 traktors2Koord;
+	[HideInInspector]
+	public Vector2 ugunsKoord;
 
 	public Canvas kanva;
 
@@ -34,5 +61,32 @@ public class Objekti : MonoBehaviour {
 
         bussKoord =
 		autobuss.GetComponent<RectTransform>().localPosition;
+
+		b2Koord =
+		b2.GetComponent<RectTransform>().localPosition;
+
+		cemenMKoord =
+		cementuMasina.GetComponent<RectTransform>().localPosition;
+
+		e46Koord =
+		e46.GetComponent<RectTransform>().localPosition;
+
+		e61Koord =
+		e61.GetComponent<RectTransform>().localPosition;
+
+		ekskavKoord =
+		ekskavators.GetComponent<RectTransform>().localPosition;
+
+		policijaKoord =
+		policija.GetComponent<RectTransform>().localPosition;
+
+		traktorsKoord =
+		traktors.GetComponent<RectTransform>().localPosition;
+
+		traktors2Koord =
+		traktors2.GetComponent<RectTransform>().localPosition;
+
+		ugunsKoord =
+		ugunsdzeseji.GetComponent<RectTransform>().localPosition;
     }
 }
